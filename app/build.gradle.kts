3346fc9c113555e4c4865093ce1e50495810c198
Davide Laezza <truzzialrogo@gmx.com>
2021-12-30 21:10:26 +0100

    build(tools): add ktlint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.5.31"
 
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+
     // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
